08c198a168309a2cd1a1c4e797b279fef252df24
dytra <vancielnugraha@gmail.com>
2020-03-01 23:34:40 +0700

    finish styling react datepicker calendar

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -3,7 +3,7 @@ import charticon from "../assets/chart.svg";
 const SideNav = () => {
   return <nav id="side-nav">
     <ul>
-      <li className="burger"><i class="fas fa-bars"></i></li>
+      <li className="burger"><i className="fas fa-bars"></i></li>
       <li className="chartmenu"><span><img src={charticon} /></span></li>
     </ul>
   </nav>;
