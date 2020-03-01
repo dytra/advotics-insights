6378fcfb59e3b201f6f892b1b1d5ed42d3cf6987
dytra <vancielnugraha@gmail.com>
2020-03-01 19:24:16 +0700

    add reduceDataset function utility

--- src/utils/filterdataset.js
+++ src/utils/filterdataset.js
@@ -1,40 +1,35 @@
-import { isSameDay,subDays } from "date-fns";
+import { isSameDay, subDays } from "date-fns";
 
 export const filterDataset = ({ dataset, filterMode, option }) => {
   if (filterMode === 'TODAY') {
     const filtered = dataset.filter(item => {
       return isSameDay(new Date(item.date), new Date());
     });
-    const reduced = filtered.reduce((cur, next) => {
-      return {
-        date: cur.date,
-        gross: cur.gross + next.gross,
-        nett: cur.nett + next.nett,
-        apv: cur.apv + next.apv,
-        lpt: cur.lpt + next.lpt
-      }
-    });
+    const reduced = reduceDataset(filtered);
+
     return [reduced];
 
   } else if (filterMode === 'YESTERDAY') {
     const filtered = dataset.filter(item => {
-      return isSameDay(new Date(item.date), subDays(new Date(),1));
+      return isSameDay(new Date(item.date), subDays(new Date(), 1));
     });
-    const reduced = filtered.reduce((cur, next) => {
-      return {
-        date: cur.date,
-        gross: cur.gross + next.gross,
-        nett: cur.nett + next.nett,
-        apv: cur.apv + next.apv,
-        lpt: cur.lpt + next.lpt
-      }
-    });
+    const reduced = reduceDataset(filtered);
     return [reduced];
 
   }
 }
 
-const filterOnly1Date = () => {
+const reduceDataset = (dataset) => {
+  const reduced = dataset.reduce((cur, next) => {
+    return {
+      date: cur.date,
+      gross: cur.gross + next.gross,
+      nett: cur.nett + next.nett,
+      apv: cur.apv + next.apv,
+      lpt: cur.lpt + next.lpt
+    }
+  });
+  return reduced;
 
 }
 
@@ -43,7 +38,7 @@ export const getDatasetLabels = (filterMode) => {
   let labels;
   if (filterMode === 'TODAY') {
     labels = ["Today"]
-  } else if  (filterMode === 'YESTERDAY') {
+  } else if (filterMode === 'YESTERDAY') {
     labels = ["Yesterday"]
   }
   return labels;
